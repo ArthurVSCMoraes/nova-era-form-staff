dcf0d63b8f7722c71e915703e6733cbbeabe7b6a
Arthur Moraes <102390235+ArthurVSCMoraes@users.noreply.github.com>
2024-09-15 02:38:39 -0300

    Uptade data resp with Don and Russo

--- src/data/staffResp.js
+++ src/data/staffResp.js
@@ -2,7 +2,9 @@ const staffResp = [
     { id: 1, name: "Vulcano", discId: "660278234428080138" },
     { id: 2, name: "Gigi", discId: "942564213656797224" },
     { id: 3, name: "Jl", discId: "750076986231226418" },
-    { id: 4, name: "Bené", discId: "609603315289948174",}
+    { id: 4, name: "Bené", discId: "609603315289948174",},
+    { id: 5, name: "Don", discId: "701571876584947804",},
+    { id: 6, name: "Russo", discId: "1205920159831687258",}
   ];
 
 

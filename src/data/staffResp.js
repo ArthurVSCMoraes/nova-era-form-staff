67d53cc5a3bf4a6bfaefc4d03d4ff85705d1d9fb
Arthur Moraes <102390235+ArthurVSCMoraes@users.noreply.github.com>
2024-10-16 20:24:03 -0300

    Update staffResp.js

--- src/data/staffResp.js
+++ src/data/staffResp.js
@@ -1,10 +1,14 @@
 const staffResp = [
-    { id: 1, name: "Vulcano", discId: "660278234428080138" },
-    { id: 2, name: "Gigi", discId: "942564213656797224" },
-    { id: 3, name: "Jl", discId: "750076986231226418" },
-    { id: 4, name: "Bené", discId: "609603315289948174",},
+    { id: 1, name: "Gigi", discId: "942564213656797224" },
+    { id: 2, name: "Vulcano", discId: "660278234428080138" },
+    { id: 3, name: "Di`bel", discId: "571854287014068243" },
+    { id: 4, name: "Mikzinn", discId: "358879618943680513",},
     { id: 5, name: "Paiva", discId: "1181458899962970132",},
-    { id: 6, name: "Leandro", discId: "915296754939854888",}
+    { id: 6, name: "Santos", discId: "1159921981714599946",},
+    { id: 7, name: "Dubronx", discId: "1190517700288581653",},
+    { id: 8, name: "Laura", discId: "1166819494858084363",},
+    { id: 9, name: "Araujo", discId: "918130354785771550",},
+    { id: 9, name: "Yunna", discId: "500171348769898498",}
   ];
 
 

a0ba2e0f7af7e63f03a03dc527b152ecb784bbfa
Arthur Moraes <102390235+ArthurVSCMoraes@users.noreply.github.com>
2024-12-09 20:26:41 -0300

    Update staffResp.js

--- src/data/staffResp.js
+++ src/data/staffResp.js
@@ -2,8 +2,8 @@ const staffResp = [
     { id: 1, name: "Gigi", discId: "942564213656797224" },
     { id: 2, name: "Vulcano", discId: "660278234428080138" },
     { id: 3, name: "Di`bel", discId: "571854287014068243" },
-    { id: 4, name: "Mikzinn", discId: "358879618943680513",},
-    { id: 5, name: "Paiva", discId: "1181458899962970132",},
+    { id: 4, name: "Andrezinho", discId: "548630874204143660",},
+    { id: 5, name: "Miranha", discId: "740256657077960739",},
     { id: 6, name: "Santos", discId: "1159921981714599946",},
     { id: 7, name: "Dubronx", discId: "1190517700288581653",},
     { id: 8, name: "Laura", discId: "1166819494858084363",},
@@ -12,10 +12,16 @@ const staffResp = [
     { id: 11, name: "Barão", discId: "1294681992843235469",},
     { id: 12, name: "Agustinho", discId: "755164641633501474",},
     { id: 13, name: "NotFake", discId: "709086381883916308",},
-    { id: 14, name: "DUDU", discId: "936302970621022278",},
-    { id: 15, name: "Dudu Santos", discId: "1185345880115646609",},
+    { id: 14, name: "Meruem", discId: "621834290673745920",},
+    { id: 15, name: "Athos", discId: "1229894383235502081",},
     { id: 16, name: "Denta", discId: "713523297668825121",},
-    { id: 17, name: "Jully", discId: "595771979592892457",}
+    { id: 17, name: "Jully", discId: "595771979592892457",},
+    { id: 18, name: "Palmeira", discId: "351524943701344273",},
+    { id: 19, name: "Fred", discId: "1300305974548107274",},
+    { id: 20, name: "JP", discId: "890976344865906688",},
+    { id: 21, name: "Gatti", discId: "1177706897365798966",},
+    { id: 22, name: "Loirinho", discId: "1288363051812388949",},
+    { id: 23, name: "Barão", discId: "1294681992843235469",}
   ];
 
 

853364364d4862310c0cc2ec41620e78bf090196
Arthur Moraes <102390235+ArthurVSCMoraes@users.noreply.github.com>
2025-10-31 00:00:14 -0300

    Fix syntax error in staffResp.js

--- src/data/staffResp.js
+++ src/data/staffResp.js
@@ -1,6 +1,7 @@
 const staffResp = [
     { id: 1, name: "Vulcano", discId: "660278234428080138" },
-    { id: 2, name: "Nat", discId: "618278533537595442" }
+    { id: 2, name: "Nat", discId: "618278533537595442" },
+    { id: 3, name: "Pertubado", discId: "1256703547194740741" }
   ];
 
 

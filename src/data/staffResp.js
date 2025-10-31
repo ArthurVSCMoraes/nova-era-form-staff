7628370e81d564d16943f633571494eeb4cda829
Arthur Moraes <102390235+ArthurVSCMoraes@users.noreply.github.com>
2025-10-31 00:12:48 -0300

    Add new staff members to staffResp array

--- src/data/staffResp.js
+++ src/data/staffResp.js
@@ -1,7 +1,10 @@
 const staffResp = [
     { id: 1, name: "Vulcano", discId: "660278234428080138" },
     { id: 2, name: "Nat", discId: "618278533537595442" },
-    { id: 3, name: "Pertubado", discId: "1256703547194740741" }
+    { id: 3, name: "Pertubado", discId: "1256703547194740741" },
+    { id: 4, name: "Haii", discId: "690971355713110096" },
+    { id: 5, name: "Su", discId: "838855418364166185" },
+    { id: 6, name: "NTREM", discId: "332660334064173058" }
   ];
 
 

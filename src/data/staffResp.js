680c9c98ac92077173b06f6773ab002a1ea5fd58
Arthur Moraes <102390235+ArthurVSCMoraes@users.noreply.github.com>
2025-12-14 17:10:09 -0300

    Add new staff member 'JUNIN' to staffResp

--- src/data/staffResp.js
+++ src/data/staffResp.js
@@ -4,7 +4,8 @@ const staffResp = [
     { id: 3, name: "Pertubado", discId: "1256703547194740741" },
     { id: 4, name: "Haii", discId: "690971355713110096" },
     { id: 5, name: "Su", discId: "838855418364166185" },
-    { id: 6, name: "NTREM", discId: "332660334064173058" }
+    { id: 6, name: "NTREM", discId: "332660334064173058" },
+    { id: 7, name: "JUNIN", discId: "1200008509899690099" }
   ];
 
 
